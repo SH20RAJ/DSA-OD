ae64fb5e6468b1d75dffe185f893384ba01adca3
SH20RAJ <shaswatraj3@gmail.com>
2025-06-12 15:13:08 +0530

    Update binary search implementation: modify target value and expand array for testing

--- concepts/binary-search.cpp
+++ concepts/binary-search.cpp
@@ -4,25 +4,25 @@ using namespace std;
 
 int main(){
     
-    vector<int> arr = { 1,3,5,78,90};
+    vector<int> arr = { 1,3,5,78,90,799,999};
 
-    int target = 5;
+    int target = 90;
     int size = arr.size(), l = 0 , r = size -1;
     while(l<r){
-       int mid = (l+r)/2;
+        int mid = (l+r)/2;
+        // cout << l << " " << r << " " << mid << endl ;
 
        if(arr.at(mid) == target){
         cout << "Target Found at " << mid << endl;
         return 1;
        } else if(arr.at(mid) > target){
         r = mid;
-       } else {
+       } else if(arr.at(mid) < target){
         l = mid;
+
        }
+    //    return -1;
     }
     
-    
-    
-    
-return 0;
+    return 0;
 }
