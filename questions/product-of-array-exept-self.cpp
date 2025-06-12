2afa3cfc1b02ea2b5660fc84605b24c7f5989d76
SH20RAJ <shaswatraj3@gmail.com>
2025-06-12 14:30:16 +0530

    Refactor "Product of Array Except Self" implementation for clarity and correctness

--- questions/product-of-array-exept-self.cpp
+++ questions/product-of-array-exept-self.cpp
@@ -1,35 +1,55 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
 using namespace std;
 
-int main(){
-    vector<int> arr = {1,2,3,4};
-    vector<int> aarr = arr;
+int main()
+{
+    vector<int> arr = {1, 2, 3, 4}; // 24 , 12 , 8 , 6
+    int size = arr.size();
+    vector<int> ans(size, 1);
 
+    cout << " 1, 2 , 3, 4" << endl;
 
     // Brut Approach
 
-    for (int i = 0; i < arr.size(); i++)
+    // for (int i = 0; i < arr.size(); i++)
+    // {
+    //     aarr[i] = 1;
+    //     for (int j = 0; j < arr.size(); j++)
+    //     {
+    //        (i!=j)?aarr[i]*=arr[j]:NULL;
+    //     }
+
+    // }
+
+    // Optimal Approach
+
+    // prefix 1 1 2 6
+    for (int i = 1; i < size; i++)
     {
-        aarr[i] = 1;
-        for (int j = 0; j < arr.size(); j++)
-        {
-           (i!=j)?aarr[i]*=arr[j]:NULL;
-        }
-        
+        ans[i] = ans[i - 1] * arr.at(i - 1);
     }
 
+    for (int i = 0; i < size; i++)
+    {
+        cout << ans[i] << " , ";
+    }
+
+    cout << endl;
+    // suffix 
+
+    for (int i = size - 2; i > 0; i--)
+    {
+        ans[i] = ans[i+1] * arr[i + 1];
+        cout << "answer in loop " << ans[i] << " , " << i << " " << arr[i]<< endl;
+    }
 
     // print answer
 
-    for (int i = 0; i < arr.size(); i++)
+    for (int i = 0; i < size; i++)
     {
-       cout << aarr[i] << " , " ;
+        cout << ans[i] << " , ";
     }
-    
 
-    
-    
-    
-return 0;
+    return 0;
 }
