337591771563c5d2c6b345da0b94ea08e234bd62
SH20RAJ <shaswatraj3@gmail.com>
2025-06-12 13:22:48 +0530

    Refactor code structure for improved readability and maintainability

--- tests/palindrome.cpp
+++ tests/palindrome.cpp
@@ -1,10 +1,28 @@
 #include<iostream>
-#include<bits/stdc++.h>
 
 using namespace std;
 
 int main(){
-    
+    vector<int> no = {1,2,1};
+    int n = 1234;
+    int n2 = n;
+
+    int rn = 0;
+    int p = 1;
+    while(n>0){
+        rn*=10;
+        rn+=n%10;
+        cout << rn << " n = "<< n << endl;
+        n/=10;
+        p*=10;
+    }
+
+    cout << rn << endl;
+    if(rn == n2){
+        cout << "palindrome" << endl;
+    } else {
+        cout << " no palindrome" << endl;
+    }
     
     
     
